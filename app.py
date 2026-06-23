7b3bb69839274ea9bcd02139d1c375e22c80765d
maritzlindy1-hue <maritz.lindy1@gmail.com>
2026-06-23 12:34:45 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, send_file
-
-app = Flask(__name__)
-
-@app.route("/")
-def dashboard():
-    return send_file("dashboard.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+from flask import Flask,send_file
+app=Flask(__name__)
+@app.route('/')
+def home():
+ return send_file('dashboard.html')
